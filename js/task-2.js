31321b0908740140573fee3bf1c5e4eebe7efedd
Asya-Sh <str.injuly@gmail.com>
2024-02-05 17:07:17 +0200

    corrected-tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,12 @@
-// function formatMessage(message, maxLength) {
-//   return message.length <= maxLength
-//     ? message
-//     : `${message.slice(0, maxLength)}...`;
-// }
+function formatMessage(message, maxLength) {
+  return message.length <= maxLength
+    ? message
+    : `${message.slice(0, maxLength)}...`;
+}
 
-// console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-// console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-// console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-// console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
